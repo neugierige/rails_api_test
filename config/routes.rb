0c2e06b2db5fc194f2b5a554b42fb9e22dd013a7
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-22 22:51:03 -0400

    configuring the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    constraints: { subdomain: 'api' }, path: '/' do
-      # list resources here
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
+      #list resources here
+    end
   end
 end
+
+
