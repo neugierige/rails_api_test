07115fcb1f98be299e9d81295af3dca5d0a8ae8b
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-24 19:50:41 -0400

    added user destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ RailsApiTest::Application.routes.draw do
   														constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, 
     			constraints: ApiConstraints.new(version: 1, default: true) do
-      	resources :users, :only => [:show, :create, :udpate]
+      	resources :users, :only => [:show, :create, :udpate, :destroy]
     end
   end
 end
